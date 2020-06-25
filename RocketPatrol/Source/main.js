afb98c912cbd4d1a38a694f7b2c7672fb92c1deb
Izze <56184561+i-cortez@users.noreply.github.com>
2020-06-25 14:25:37 -0700

    Uploading files from todays class example

---
+++ RocketPatrol/Source/main.js
@@ -0,0 +1,9 @@
+// Create game configuration object
+let config =
+{
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480
+};
+
+let game = new Phaser.Game(config); // create main game object
