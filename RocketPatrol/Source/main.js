c2c95b4c8977d742836153eda0d0ea419a2d81ee
Izze <56184561+i-cortez@users.noreply.github.com>
2020-06-25 16:39:59 -0700

    Including files from in class example

--- RocketPatrol/Source/main.js
+++ RocketPatrol/Source/main.js
@@ -3,7 +3,11 @@ let config =
 {
     type: Phaser.CANVAS,
     width: 640,
-    height: 480
+    height: 480,
+    scene: [Menu, Play]
 };
 
 let game = new Phaser.Game(config); // create main game object
+
+// reserve some keyboard bindings
+let keyF, keyLEFT, keyRIGHT;
